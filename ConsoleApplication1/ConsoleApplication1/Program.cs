3dc5515e8fd58a6980a345ba410954bb3534f547
ashakotha <asha.kotha@tcs.com>
2016-07-04 02:54:06 -0400

    Addition of nums

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b,sum;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+            Console.WriteLine("the addition of two numbers is" + sum);
+
+        }
+    }
+}
